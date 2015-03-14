4904f8a1f16ae2f7127ec085d7749574405d31a1
SwaroopG <swarooprajg@gmail.com>
2015-03-14 16:39:20 -0500

    Minor formatting fixes to SQLite Dialect

--- src/main/java/org/hibernate/dialect/SQLiteDialect.java
+++ src/main/java/org/hibernate/dialect/SQLiteDialect.java
@@ -6,12 +6,11 @@ package org.hibernate.dialect;
 
 import java.sql.Types;
 
-import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.VarArgsSQLFunction;
 import org.hibernate.type.IntegerType;
 import org.hibernate.type.StringType;
-import org.hibernate.Hibernate;
 
 public class SQLiteDialect extends Dialect
 {
@@ -47,6 +46,7 @@ public class SQLiteDialect extends Dialect
         registerFunction("substring", new StandardSQLFunction("substr", StringType.INSTANCE));
     }
 
+    @Override
     public boolean supportsIdentityColumns()
     {
         return true;
@@ -56,6 +56,7 @@ public class SQLiteDialect extends Dialect
      * public boolean supportsInsertSelectIdentity() { return true; // As specify in NHibernate dialect }
      */
 
+    @Override
     public boolean hasDataTypeInIdentityColumn()
     {
         return false; // As specify in NHibernate dialect
@@ -66,108 +67,129 @@ public class SQLiteDialect extends Dialect
      * NHibernate dialect append(insertString). append("; ").append(getIdentitySelectString()). toString(); }
      */
 
+    @Override
     public String getIdentityColumnString()
     {
         // return "integer primary key autoincrement";
         return "integer";
     }
 
+    @Override
     public String getIdentitySelectString()
     {
         return "select last_insert_rowid()";
     }
 
+    @Override
     public boolean supportsLimit()
     {
         return true;
     }
 
+    @Override
     protected String getLimitString(String query, boolean hasOffset)
     {
         return new StringBuffer(query.length() + 20).append(query).append(hasOffset ? " limit ? offset ?" : " limit ?").toString();
     }
 
+    @Override
     public boolean supportsTemporaryTables()
     {
         return true;
     }
 
+    @Override
     public String getCreateTemporaryTableString()
     {
         return "create temporary table if not exists";
     }
 
+    @Override
     public boolean dropTemporaryTableAfterUse()
     {
         return false;
     }
 
+    @Override
     public boolean supportsCurrentTimestampSelection()
     {
         return true;
     }
 
+    @Override
     public boolean isCurrentTimestampSelectStringCallable()
     {
         return false;
     }
 
+    @Override
     public String getCurrentTimestampSelectString()
     {
         return "select current_timestamp";
     }
 
+    @Override
     public boolean supportsUnionAll()
     {
         return true;
     }
 
+    @Override
     public boolean hasAlterTable()
     {
         return false; // As specify in NHibernate dialect
     }
 
+    @Override
     public boolean dropConstraints()
     {
         return false;
     }
 
+    @Override
     public String getAddColumnString()
     {
         return "add column";
     }
 
+    @Override
     public String getForUpdateString()
     {
         return "";
     }
 
+    @Override
     public boolean supportsOuterJoinForUpdate()
     {
         return false;
     }
 
+    @Override
     public String getDropForeignKeyString()
     {
         throw new UnsupportedOperationException("No drop foreign key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable, String[] primaryKey,
             boolean referencesPrimaryKey)
     {
         throw new UnsupportedOperationException("No add foreign key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public String getAddPrimaryKeyConstraintString(String constraintName)
     {
         throw new UnsupportedOperationException("No add primary key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public boolean supportsIfExistsBeforeTableName()
     {
         return true;
     }
 
+    @Override
     public boolean supportsCascadeDelete()
     {
         return false;
